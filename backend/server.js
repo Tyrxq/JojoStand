997967e12b91f90ec535e6e6bbf422613c475ced
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-03-30 02:26:21 -0500

    Now saves images in a folder

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
-
+const fs = require('fs');
 
 
 const key = process.env.OPENAI_API_KEY
-console.log(key)
+
 
 const configuration = new Configuration({
     apiKey: key
@@ -13,13 +13,28 @@ const openai = new OpenAIApi(configuration);
 
 const predict = async function() {
     const response = await openai.createImage({
-        prompt: "A realistic photo of a generated stand from JoJo's Bizarre Adventure with a black background and no other objects in frame. Stand Appearance: Time Warp takes the form of a humanoid figure with a clock-like design on its body, complete with spinning gears and ticking hands. It wears a long coat made of shimmering time energy, which trails behind it as it moves ",
+        prompt: "A realistic photo of a generated stand from JoJo's Bizarre Adventure with a black background,no crop and no other objects in frame. Stand Appearance: 「Timeless Dragon」 takes the form of a sleek and powerful dragon made entirely of shimmering, metallic silver. Its eyes glow with a fierce blue light, and its wingspan stretches to nearly 20 feet. The Stand's scales are intricately detailed, giving it the appearance of a living work of art. ",
         n: 3,
-        size: "1024x1024"
+        size: "512x512",
+        response_format:'b64_json',
     });
-    console.log(response.data);
+
 
     return response.data;
 }
 
-predict();
+predict().then(
+    response => {
+        const now = Date.now();
+     
+        for(let i = 0; i < response.data.length; i++)
+        {
+            const b64 = response.data[i]['b64_json'];
+            const buffer = Buffer.from(b64,"base64");
+            const filename = `images/image_${now}_${i}.png`;
+            console.log("Writing image" + filename)
+            fs.writeFileSync(filename,buffer);           
+        }
+      
+    }
+)
