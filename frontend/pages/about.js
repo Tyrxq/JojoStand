f46ea2b003c6db0aaf4ab65723c3f1e744fdca5b
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-04-07 02:39:29 -0500

    Website serverSideProps

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,13 +1,23 @@
-import Link from 'next/link'
-
-export default function About(){
-    return(
-
-        <div>
-            <h1>About Page</h1>
-            <Link href = "/">Go back to Home</Link>
-        </div>
-    )   
-}
-
+export default function About(props) {
+    return (
+      <>
+        <h2>About Us</h2>
+        <p>Welcome to this amazing about page. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro dolore officiis atque voluptas, quas, repellendus cum, magnam a alias unde reiciendis voluptates aliquam maxime laborum? Quae omnis eius impedit et?</p>
+        <p>I have {props.repoCount} public repos on GitHub.</p>
+      </>
+    )
+  }
+  
+  export async function getStaticProps() {
+    const response = await fetch("https://api.github.com/users/Tyrxq")
+    const data = await response.json()
+  
+    return {
+      props: {
+        repoCount: data.public_repos
+      },
+      //request the data again after 10 secs then rebuild. Update will show after refresh  
+      revalidate: 10
+    }
+  }
 
