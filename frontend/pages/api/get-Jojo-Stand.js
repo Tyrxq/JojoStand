63bde3a9731672930d58a3212ae437c7737a7da1
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-04-10 10:58:45 -0500

    api creates a request

--- frontend/pages/api/get-Jojo-Stand.js
+++ frontend/pages/api/get-Jojo-Stand.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Configuration, OpenAIApi } = require("openai");
-const fs = require('fs');
+
 
 
 const key = process.env.OPENAI_API_KEY
@@ -12,68 +12,31 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 
- async function predict(appearance) {
-    const response = await openai.createImage({
-        prompt: "A realistic photo of a generated stand from JoJo's Bizarre Adventure with a black background,anime key visual of Star Dust Platinum and no other objects in frame. Stand Appearance: " + appearance ,
-        n: 3,
-        size: "512x512",
-        response_format:'b64_json',
-    });
 
-    console.log(appearance);
-    return response.data;
-}
+export default async function handler(req,res){
 
- async function generate(standPicture) {
     const response = await openai.createCompletion({
         model: "text-davinci-003",
         prompt:`Generate your own random stand power from JoJo bizarre adventure in this json format 
         {
-            "Name": "",
-            "Stats": {
-                "Destructive Power": "",
-                "Speed": "",
-                "Range": "",
-                "Durability": ""
+            "name": "",
+            "stats": {
+                "destructivePower": "",
+                "speed": "",
+                "range": "",
+                "durability": ""
             },
-            "Ability": "",
-            "Appearance": "",
-            "Weakness": ""
+            "ability": "",
+            "appearance": "",
+            "weakness": ""
             }
         }
-        ,`,
+        `,
         temperature: .75,
         max_tokens: 1000
     });
-    console.log(response.data.choices[0].text);
 
-    jsonObject = JSON.parse(response.data.choices[0].text)
-
-    standPicture(jsonObject.Appearance).then(
-        response => {
-            const now = Date.now();
-         
-            for(let i = 0; i < response.data.length; i++)
-            {
-                const b64 = response.data[i]['b64_json'];
-                const buffer = Buffer.from(b64,"base64");
-                const filename = `images/image_${now}_${i}.png`;
-                console.log("Writing image" + filename)
-                fs.writeFileSync(filename,buffer);           
-            }
-          
-        }
-    )
-
-    return [jsonObject,response];
-}
-
-//generate(predict);
-
-export default async function handler(req,res){
-
-    const response = await generate(predict);
-
-    res.status(200).json({ standInfo: response[0],standPics:response[1].data.data[0].url });
+   
+    res.status(200).json({text: response.data.choices[0].text});
 
 }
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,77 @@
+import {useState} from "react"
+
 
 export default function Home(props) {
-    return (
-      <div>
-        <h2>Welcome to our homepage.</h2>
-        <p>This is the best home page in the world. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum aspernatur illum architecto cumque recusandae fuga sequi necessitatibus deleniti repellat harum nobis, dolor veniam vero deserunt. Voluptatibus, ducimus deserunt. Recusandae, dolore.</p>
-        <p>The weather: {props.forecast}</p>
+    
+    const [jojoStand,setJostand] = useState({
+       
+      });
+  
+
+  
+    async function submit(e){
+      e.preventDefault();
+
+      const response = await fetch("/api/get-Jojo-Stand",{
+        method: "GET",
+        headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+        },
+      })
+
+      const stand = await response.json()
+      setJostand(stand)
+      console.log(jojoStand.text)
+      
+      
+
+    }
+    
+
+
+
+    return(
+      <div className = "container">
+        
+        <form onSubmit = {submit}>
+          
+          <button type = "submit">Generate your Jojo Stand</button>
+
+
+        </form>
+        
+        {/* 
+        <div className ="jojo-info">
+
+          <h2>{jojoStand["standInfo"].Name}</h2>
+          
+          <h3>Ability</h3>
+          
+          <h4>{jojoStand["standInfo"].Ability}</h4>
+          
+          <h3>Stats</h3>
+          
+          <p>Destructive Power:{jojoStand["standInfo"].Stats["Destructive Power"]}</p>
+          
+          <p>Speed:{jojoStand["standInfo"].Stats["Speed"]}</p>
+          
+          <p>Range:{jojoStand["standInfo"].Stats["Range"]}</p>
+          
+          <p>Durability:{jojoStand["standInfo"].Stats["Durability"]}</p>
+          
+          <h3>Weakness</h3>
+          
+          <h4>{jojoStand["standInfo"].Weakness}</h4>
+
+          <img src={jojoStand.standPics} />
+
+
+        </div>
+        */}
+        
       </div>
     )
-  }
+}
  
  
