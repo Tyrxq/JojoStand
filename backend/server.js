6a50cd3a1c46550eb163e841630d66ddc7a92f7f
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-04-12 15:36:45 -0500

    backend can take prompts from user

--- backend/server.js
+++ backend/server.js
@@ -90,7 +90,51 @@ async function gptGenerates(){
         ,`}],
         max_tokens:1000,
       });
-      console.log(completion.data.choices[0].message.content);
+      
 }
 
-gptGenerates()
+
+async function gptGenerate(standPicture){
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{role: "system", content: `Generate your own random stand power from JoJo bizarre adventure in this json format from the user's personality and mental strength. And the ability should be described with 25 words or more. You must only respond with this json format 
+        {
+            "Name": "",
+            "Stats": {
+                "Destructive Power": "",
+                "Speed": "",
+                "Range": "",
+                "Durability": ""
+            },
+            "Ability": "",
+            "Appearance": "",
+            "Weakness": ""
+            }
+        }
+        ,`},{role:"user",content: "Hello, I'm tyriq. I enjoy skate boarding,anime,programing,singing,dancing,chess,basketball, going to the gym and hanging out with friends. I'm currently in college studying computer science and a project manager of an organization that makes satelites. I am also a follower of Jesus Christ and take my faith seriously."}],
+        max_tokens:1000,
+      });
+      console.log(completion.data.choices[0].message.content);
+      
+      jsonObject = JSON.parse(completion.data.choices[0].message.content)
+
+      standPicture(jsonObject.Appearance).then(
+          response => {
+              const now = Date.now();
+           
+              for(let i = 0; i < response.data.length; i++)
+              {
+                  const b64 = response.data[i]['b64_json'];
+                  const buffer = Buffer.from(b64,"base64");
+                  const filename = `images/image_${now}_${i}.png`;
+                  console.log("Writing image" + filename)
+                  fs.writeFileSync(filename,buffer);           
+              }
+            
+          }
+      )
+  
+      return jsonObject;
+}
+
+gptGenerate(predict)
