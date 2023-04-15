02e81a7a18d2e9c274dfd3be62752796f69d19bc
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-04-15 11:39:24 -0500

    Changed postion of loading icon

--- frontend/components/Textbox.js
+++ frontend/components/Textbox.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Textbox = ({submit,func}) => {
+const Textbox = ({submit,func,loading}) => {
   return (
     <form class = "text-container" onSubmit = {submit}>
+       
         <textarea id = "textbox" placeholder='Give details about yourself' onChange={(e) => func(e.target.value)}/>
         <button id = "text-button" type = "submit">Generate Stand</button>
+        {loading && <div className="loading-spinner"></div>}
   </form>
   )
 }
