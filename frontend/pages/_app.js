1e96c768911b2a8fb552a4cad9308d093d765dc5
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-04-30 00:11:25 -0500

    added vercel analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,7 +3,9 @@ import "../styles/index.css";
 import "../styles/textbox.css";
 import "../styles/jojoInfo.css";
 import  "../styles/pics.css";
-import {useRouter} from 'next/router'
+import {useRouter} from 'next/router';
+import { Analytics } from '@vercel/analytics/react';
+;
 
 export default function App({Component, pageProps}){
     const router = useRouter();
@@ -11,6 +13,7 @@ export default function App({Component, pageProps}){
         <>
            
             <Component {...pageProps}/>
+            <Analytics/>
         
         </>
 
