cda02c4a50f2852827484b89180d31e7328f05bc
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-05-25 17:16:43 -0500

    Added title

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <link rel ="shortcut icon" href='/favicon/favicon.ico'/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"></link>
+        <title>Jojo's Stand Arrow</title>
       </Head>
       <body>
         <Main />
