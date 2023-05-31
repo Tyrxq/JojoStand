d51de1806f6292abc6206f6711b630a76a4c7598
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-05-30 23:21:45 -0500

    removed links from navbar

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -8,27 +8,13 @@ const Navbar = () => {
 
     return (
         <nav className="navbar navbar-expand-lg bg-body-tertiary nav-backround navbar-dark" >
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+            
             
             <div className="container d-flex justify-content-center">
                
                 <div className="row">
-                    <div className="col-12 d-flex justify-content-center mb-3">
+                    <div className="col-12 d-flex justify-content-center ">
                         <h2 id = "navbar-title">Jojo's Stand Arrow</h2>
-                    </div>
-
-                    <div className="col-12 d-flex justify-content-center"> 
-                        <div className="collapse navbar-collapse links" id="navbarToggleExternalContent">
-                            <ul className="navbar-nav align-items-center mx-auto">
-
-                                <Link className={currentpage == "/" ?"nav-link nav-text": "nav-link"} aria-current="page" href="/">Generation</Link>
-                                
-                                <Link className={currentpage == "/stands" ?"nav-link nav-text": "nav-link"} href="/stands">Stands</Link>
-                                
-                            </ul>
-                        </div>
                     </div>
                     
                 </div>
