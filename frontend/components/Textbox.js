4c8391c9a5c7c4f23f93fdf8a28209bd36cc2649
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-05-30 23:17:38 -0500

    Changed the loading bar

--- frontend/components/Textbox.js
+++ frontend/components/Textbox.js
@@ -4,7 +4,7 @@ const Textbox = ({submit,func,loading,pics}) => {
   return (
     <form className = "text-container" onSubmit = {submit}>
        
-        <textarea id = {pics.length === 0 ? "textbox-nothing" : "textbox"} placeholder="Type in details about yourself" onChange={(e) => func(e.target.value)}/>
+        <textarea className = {loading && "loading-text"}id = {pics.length === 0 ? "textbox-nothing" : "textbox"} placeholder="Type in details about yourself" onChange={(e) => func(e.target.value)}/>
         <button id = {pics.length === 0 ? "text-button-nothing" : "text-button"} type = "submit">Generate </button>
         {loading && <div className="loading-spinner"></div>}
   </form>
