33f81379682bfe5311b7e9a47d4cc633284746f7
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-05-31 11:56:48 -0500

    fixed title error and removed LInk/router

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -4,13 +4,17 @@ import "../styles/textbox.css";
 import "../styles/jojoInfo.css";
 import  "../styles/pics.css";
 import "../styles/navbar.css";
-import {useRouter} from 'next/router';
 import { Analytics } from '@vercel/analytics/react';
 import Navbar from "../components/Navbar";
+import Head from 'next/head'
+
 
 export default function App({Component, pageProps}){
     return(
         <>
+            <Head>
+                <title>Jojo's Stand Arrow</title>
+            </Head>
             <Navbar/>
             <Component {...pageProps}/>
             <Analytics/>
