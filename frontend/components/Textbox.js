e8e8d4bd49587ad42e6259c5474ecb775a1b579c
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-06-01 19:18:11 -0500

    removed spinner

--- frontend/components/Textbox.js
+++ frontend/components/Textbox.js
@@ -6,7 +6,6 @@ const Textbox = ({submit,func,loading,pics}) => {
        
         <textarea className = {loading === true ? "loading-text" : "normal-text"}id = {pics.length === 0 ? "textbox-nothing" : "textbox"} placeholder="Type in details about yourself" onChange={(e) => func(e.target.value)}/>
         <button id = {pics.length === 0 ? "text-button-nothing" : "text-button"} type = "submit">Generate </button>
-        {loading && <div className="loading-spinner"></div>}
   </form>
   )
 }
