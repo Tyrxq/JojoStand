252aea0e9a4b61fd0a0f513b302e8024f3e2815e
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-06-08 20:04:25 -0500

    Textbox is now more responsive

--- frontend/components/Textbox.js
+++ frontend/components/Textbox.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Textbox = ({submit,func,loading,pics}) => {
   return (
-    <form className = "text-container" onSubmit = {submit}>
+    <form className = {pics.length === 0 ? "text-container-nothing" : "text-container"} onSubmit = {submit}>
        
         <textarea className = {loading === true ? "loading-text" : "normal-text"}id = {pics.length === 0 ? "textbox-nothing" : "textbox"} placeholder="Type in details about yourself" onChange={(e) => func(e.target.value)}/>
         <button id = {pics.length === 0 ? "text-button-nothing" : "text-button"} type = "submit">Generate </button>
