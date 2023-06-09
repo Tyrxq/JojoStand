f73a6f1d32ff5abd9946d785744a52478d6e5bb1
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-06-08 20:42:56 -0500

    added a span to button

--- frontend/components/Textbox.js
+++ frontend/components/Textbox.js
@@ -5,7 +5,7 @@ const Textbox = ({submit,func,loading,pics}) => {
     <form className = {pics.length === 0 ? "text-container-nothing" : "text-container"} onSubmit = {submit}>
        
         <textarea className = {loading === true ? "loading-text" : "normal-text"}id = {pics.length === 0 ? "textbox-nothing" : "textbox"} placeholder="Type in details about yourself" onChange={(e) => func(e.target.value)}/>
-        <button id = {pics.length === 0 ? "text-button-nothing" : "text-button"} type = "submit">Generate </button>
+        <button id = {pics.length === 0 ? "text-button-nothing" : "text-button"} type = "submit"><span class="material-symbols-outlined">mediation</span> </button>
         {loading && <div className="loading-spinner"></div>}
   </form>
   )
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel ="shortcut icon" href='/favicon/favicon.ico'/>
         <link href="https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap" rel="stylesheet"></link>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous"></link>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
         
        
