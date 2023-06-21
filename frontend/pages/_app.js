4b11ef659b0ba57a69b3f9f12e23d5340d923d12
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2023-06-21 00:12:03 -0500

    added new meta data

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -14,6 +14,11 @@ export default function App({Component, pageProps}){
         <>
             <Head>
                 <title>StandFinderX</title>
+            <meta
+                name="description"
+                content="Unveil the extraordinary power within you and embark on a thrilling journey through the captivating realm of Jojo's Bizarre Adventure! Our interactive website invites you to explore the depths of your Stand potential like never before. Simply provide us with some general information about yourself, and allow our advanced AI to craft a personalized experience just for you. Witness the manifestation of your Stand as you receive a unique picture, detailed stats, and a mesmerizing description of its awe-inspiring abilities. Immerse yourself in the world of Stands and discover the hidden depths of your own power. Are you ready to embrace your destiny and unleash your Stand in Jojo's Bizarre Adventure? Step into the extraordinary realm of StandFinderX.com and let the adventure begin!"
+                key="desc"
+            />
             </Head>
             <Navbar/>
             <Component {...pageProps}/>
