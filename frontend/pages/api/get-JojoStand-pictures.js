c5a935ce639170581119c64e577168f0090634b1
Tyrxq <49320849+Tyrxq@users.noreply.github.com>
2024-11-30 19:31:18 -0600

    Added a new model to the call for dall-e-2

--- frontend/pages/api/get-JojoStand-pictures.js
+++ frontend/pages/api/get-JojoStand-pictures.js
@@ -9,6 +9,7 @@ export const config = {
 async function predictFetch(appearance){
 
     const payload = {
+        model:"dall-e-2",
         prompt: "A realistic photo of a generated stand from JoJo's Bizarre Adventure with a black background,anime key visual of Star Dust Platinum, full body picture and no other objects in frame. Stand Appearance: " + appearance ,
         n: 3,
         size: "512x512",
